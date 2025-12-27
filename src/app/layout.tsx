b881dfc3fd43ca4bd59b31845e2ef921c8d652bf
shouta totsuka <5h0utat0t2uka@gmail.com>
2025-12-27 17:55:01 +0900

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  console.log(process.env.TEST_ENV);
   return (
     <html lang="en">
       <body className={`${geistSans.variable}`}>{children}</body>
